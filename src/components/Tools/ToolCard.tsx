856d0d51867d1eeb4711dba34b3df7e4d3688be0
onlyoneaman <2000.aman.sinha@gmail.com>
2024-06-29 19:31:06 +0530

    More tools | Tools Link'

--- src/components/Tools/ToolCard.tsx
+++ src/components/Tools/ToolCard.tsx
@@ -1,7 +1,13 @@
-const ToolCard = ({tool}: { tool: { title: string, tag: string, image: string } }) => {
+const ToolCard = ({tool}: { tool: { title: string, tag: string, image: string, link: string } }) => {
+
+  const openLink = () => {
+    window.open(tool.link, "_blank");
+  }
+
   return (
     <div
       className="flex cursor-pointer flex-row items-center justify-start space-y-2 p-3 rounded-md gap-3 hover:bg-charleston"
+      onClick={() => openLink()}
     >
       <img
         className="w-12 h-12 rounded-full"
--- src/components/Tools/toolsData.js
+++ src/components/Tools/toolsData.js
@@ -3,16 +3,49 @@ const toolsData = [
         title: "ChatGPT",
         tag: "Productivity",
         image: "chatgpt.png",
+        link: "https://chat.openai.com/"
     },
     {
         title: "Logseq",
         tag: "Productivity",
         image: "logseq.png",
+        link: "https://logseq.com/"
     },
     {
         title: "Google Tasks",
         tag: "Productivity",
         image: "tasks.png",
+        link: "https://tasks.google.com/"
+    },
+    {
+        title: "Notion",
+        tag: "Productivity",
+        image: "notion.png",
+        link: "https://www.notion.so/"
+    },
+    {
+        title: "Buffer",
+        tag: "Post Scheduler",
+        image: "buffer.png",
+        link: "https://buffer.com/"
+    },
+    {
+        title: "Miro",
+        tag: "Collaboration",
+        image: "miro.png",
+        link: "https://miro.com/"
+    },
+    {
+        title: "Framer",
+        tag: "Design",
+        image: "framer.webp",
+        link: "https://www.framer.com/"
+    },
+    {
+        title: "Readwise Reader",
+        tag: "Reading",
+        image: "reader.png",
+        link: "https://readwise.io/"
     }
 ]
 
