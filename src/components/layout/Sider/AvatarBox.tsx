3ce6b1361df3fbca8954563f0facab6cf406fb9e
onlyoneaman <2000.aman.sinha@gmail.com>
2024-07-11 02:39:25 +0530

    Avatar Rounded Cover

--- src/components/layout/Sider/AvatarBox.tsx
+++ src/components/layout/Sider/AvatarBox.tsx
@@ -16,6 +16,7 @@ const AvatarBox = () => {
     >
       <Avatar>
         <AvatarImage
+          className="h-12 w-12 rounded-full object-cover"
           alt="User Image"
           src={"/images/user.jpg"}
         />
