04e4146038beb7d5b6b945f10b82fb3e3c388b69
onlyoneaman <2000.aman.sinha@gmail.com>
2024-07-12 03:49:46 +0530

    Remove redundant code from Footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -21,54 +21,12 @@ const LiveTime = () => {
   );
 };
 
-const FooterSection = ({ title, items }: any) => (
-  <div className="flex flex-col">
-    <h3 className="text-white font-semibold mb-2">{title}</h3>
-    <ul>
-      {items.map((item: any, index: number) => (
-        <li key={index} className="text-gray-400 hover:text-white mb-1">
-          <Link href={`/${item.toLowerCase()}`}>
-            {item}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-
 const Footer = () => {
-  const [currentTime, setCurrentTime] = useState('');
-
-  useEffect(() => {
-    const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    setCurrentTime(time);
-    // update time in 2 seconds
-
-  }, []);
-
   return (
     <footer className={`flex self-end min-w-full grow p-3 sm:p-5 flex-col border-t-[0.5px] 
     text-gray-500 border-gray-700 space-x-3 gap-2 text-xs sm:text-sm
     max-w-xl md:max-w-4xl mx-auto
     `}>
-      {/*<div className="grid grid-cols-4 gap-8 mb-8 hidden">*/}
-      {/*  <FooterSection*/}
-      {/*    title="Index"*/}
-      {/*    items={["Explore", "Boutique", "Projects", "Services", "About"]}*/}
-      {/*  />*/}
-      {/*  <FooterSection*/}
-      {/*    title="Products"*/}
-      {/*    items={["Billify", "Supply", "Linx", "CourseOS"]}*/}
-      {/*  />*/}
-      {/*  <FooterSection*/}
-      {/*    title="Resources"*/}
-      {/*    items={["Feed", "Thoughts", "Stack"]}*/}
-      {/*  />*/}
-      {/*  <FooterSection*/}
-      {/*    title="Connect"*/}
-      {/*    items={["Contact", "Twitter", "LinkedIn", "YouTube"]}*/}
-      {/*  />*/}
-      {/*</div>*/}
       <div className="flex justify-between items-center border-t border-gray-800 pt-4">
         <div className="flex items-center">
           {/*<span className="text-blue-400 mr-2">Currently</span>*/}
@@ -86,16 +44,6 @@ const Footer = () => {
 
         <LiveTime />
       </div>
-      {/*<div className="flex justify-around items-center mt-4 text-sm text-gray-400 hidden">*/}
-      {/*  <Link href="/buy-template" className="hover:text-white">*/}
-      {/*    Buy this template →*/}
-      {/*  </Link>*/}
-      {/*  <div className="flex space-x-4">*/}
-      {/*    <span>Pro Version</span>*/}
-      {/*    <span>By Canvas Supply</span>*/}
-      {/*    <span>Made in Framer</span>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </footer>
   );
 };
