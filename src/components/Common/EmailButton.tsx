5578fd7a48c8591d3c9b096aaf6cc85b1a7d1a52
onlyoneaman <2000.aman.sinha@gmail.com>
2024-07-11 18:00:14 +0530

    Email COpy Button

--- src/components/Common/EmailButton.tsx
+++ src/components/Common/EmailButton.tsx
@@ -1,22 +1,41 @@
+import React, {useState, useEffect} from 'react';
 import {Button} from "@/components/ui/button.tsx";
 import {FiCopy} from "react-icons/fi";
 
 const EmailButton = () => {
+  const [copyStatus, setCopyStatus] = useState(false);
+  const email = "2000.aman.sinha@gmail.com";
+
+  const copyEmail = async () => {
+    await navigator.clipboard.writeText(email);
+    setCopyStatus(true);
+  };
+
+  useEffect(() => {
+    if (copyStatus) {
+      const timer = setTimeout(() => {
+        setCopyStatus(false);
+      }, 3000); // reset after 3 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [copyStatus]);
 
   return (
     <Button
-      className={"text-white hover:text-oldsilver"}
+      className={copyStatus ? "text-white hover:text-green border-green-500 border-2" : "text-white hover:text-oldsilver"}
+      onClick={copyEmail}
     >
-      <span
-        className="text-sm pr-2"
-      >
-        <FiCopy
-          className="m-0"
-          size={12}
-        />
+      <span className="text-sm pr-2">
+        <FiCopy className="m-0" size={12}/>
       </span>
-      <span>
-        E-Mail
+      <span
+        className={copyStatus ? "text-green" : ""}
+        style={{
+          transition: "all 0.3s ease",
+        }}
+      >
+        {copyStatus ? "Copied!" : "E-Mail"}
       </span>
     </Button>
   )
