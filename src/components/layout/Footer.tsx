269659e2f4e63c26bdf5f25b0cacdde2fa3e19a3
onlyoneaman <2000.aman.sinha@gmail.com>
2024-07-12 04:32:27 +0530

    Time Smaller Size mobile

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -15,7 +15,7 @@ const LiveTime = () => {
   }, []);
 
   return (
-    <div className="text-lg text-white font-light w-20">
+    <div className="text-md sm:text-lg text-white font-light w-20">
       {currentTime}
     </div>
   );
