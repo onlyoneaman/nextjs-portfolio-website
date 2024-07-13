4c85a92a2489917b643e571071981db102c411a5
onlyoneaman <2000.aman.sinha@gmail.com>
2024-07-13 18:42:22 +0530

    Update AboutPage

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -13,7 +13,7 @@ const AboutPage = () => {
           title="Aman Kumar"
         />
         <h3
-          className="text-sm"
+          className="text-sm text-gray-500"
         >
           Full-Stack Dev | AI Engineer | Software | AI | LLMs | Travel | Music | F1
         </h3>
@@ -25,21 +25,27 @@ const AboutPage = () => {
         />
 
         <div
-          className="text-sm space-y-2"
+          className="text-md space-y-2"
         >
           <p>
-            Hello! My name is Aman, based out of Bengaluru and I enjoy creating things that live on the internet.
+            Hello! I am Aman, based out of Bengaluru and I enjoy creating things that make life easier for people.
             I have been playing around with code since 2018. I often write about AI, LLMs, Frontend Dev, React, Backend,
             Rails and DBs.
           </p>
-          <p>
-            Currently I am working as an independent consultant. I use my expertise in developing fast and clean
-            software
-            to help fast-growing startups build out their MVPs / Apps.
+          <p className="space-x-1">
+            <span>
+              Currently I am working as a Founding Engineer at
+            </span>
+            <a className="underline" href={"https://kay.ai"} target={"_blank"}>Kay.ai</a>
+            <span>
+              building workflow automation for insurance brokers.
+            </span>
+            <br/>
           </p>
           <p>
-            Earlier, I have built multiple products leading development and design for some of them. Some of them are
-            Dreamboat.ai, Engagebud, Influencerbit.
+            Earlier, I have built multiple products leading development and design for some of them.
+            <br />
+            Co-Founded Dreamboat.ai, Engagebud, Influencerbit.
           </p>
           <p>
             I am amazed by LLMs / AI, With LLMs, a lot of boring tasks will be automated and workflows are going to
