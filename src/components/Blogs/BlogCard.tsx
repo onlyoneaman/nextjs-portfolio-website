dc6c3556f790ae17eca92ea8efceba66073df2b7
onlyoneaman <2000.aman.sinha@gmail.com>
2024-07-14 03:44:27 +0530

    BlogCard motion

--- src/components/Blogs/BlogCard.tsx
+++ src/components/Blogs/BlogCard.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import {Card, CardHeader} from '@/components/ui/card';
 import {Blog} from "@/types/blog.ts";
 import moment from "moment";
+import { motion } from "framer-motion";
 
 type BlogCardProps = {
   post: Blog
@@ -21,46 +22,52 @@ const BlogCard = ({post}: BlogCardProps) => {
 
   return (
     <Link href={`/blogs/${slug}`}>
-      <Card
-        className="text-oldsilver bg-transparent border-transparent hover:border-zinc-800 min-w-full h-full"
+      <motion.div
+        className="w-full h-full"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
       >
-        <CardHeader
-          className="flex flex-col p-2 gap-2"
+        <Card
+          className="text-oldsilver bg-transparent border-transparent hover:border-zinc-800 min-w-full h-full"
         >
-          {(cardImage || image) ? (
-            <img
-              className="rounded-md object-cover h-24 sm:h-36 md:h-48"
-              src={cardImage || image || ""}
-              alt={title}
-            />
-          ) : (
-            <div className="min-w-36 h-24 bg-gradient-to-br from-zinc-800 to-zinc-700 rounded-md"></div>
-          )}
-          <div
-            className="flex flex-col gap-1"
+          <CardHeader
+            className="flex flex-col p-2 gap-2"
           >
-            <h2 className="font-semibold text-white line-clamp-1">
-              {title}
-            </h2>
-            <h4 className="text-xs line-clamp-2">
-              {description}
-            </h4>
-
+            {(cardImage || image) ? (
+              <img
+                className="rounded-md object-cover h-24 sm:h-36 md:h-48"
+                src={cardImage || image || ""}
+                alt={title}
+              />
+            ) : (
+              <div className="min-w-36 h-24 bg-gradient-to-br from-zinc-800 to-zinc-700 rounded-md"></div>
+            )}
             <div
-              className="flex gap-2 items-center justify-between text-xs text-zinc-400"
+              className="flex flex-col gap-1"
             >
+              <h2 className="font-semibold text-white line-clamp-1">
+                {title}
+              </h2>
+              <h4 className="text-xs line-clamp-2">
+                {description}
+              </h4>
+
+              <div
+                className="flex gap-2 items-center justify-between text-xs text-zinc-400"
+              >
               <span>
                 {moment(date).format("MMM DD, YYYY")}
               </span>
-              <span
-                className="bg-zinc-800 text-white rounded-md px-2 py-1"
-              >
+                <span
+                  className="bg-zinc-800 text-white rounded-md px-2 py-1"
+                >
                 {type}
               </span>
+              </div>
             </div>
-          </div>
-        </CardHeader>
-      </Card>
+          </CardHeader>
+        </Card>
+      </motion.div>
     </Link>
   )
 };
