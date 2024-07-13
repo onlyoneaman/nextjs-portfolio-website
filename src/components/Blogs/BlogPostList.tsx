4cb1a6f18e17e5f15c09cb97f0430a8a593deea8
onlyoneaman <2000.aman.sinha@gmail.com>
2024-07-13 20:35:23 +0530

    Mobile col1 blog

--- src/components/Blogs/BlogPostList.tsx
+++ src/components/Blogs/BlogPostList.tsx
@@ -41,7 +41,7 @@ const BlogPostList = (
         className="text-blue-500">Medium</a> profile.
       </p>
       <div
-        className="grid grid-cols-2 min-w-full gap-1"
+        className="grid grid-cols-1 sm:grid-cols-2 min-w-full gap-1"
       >
         {posts.map((post: Blog) => {
           if (post.hidden) return null;
