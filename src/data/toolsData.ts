c812fd06be059fe2fb6fc34494e8a81e39bb7df1
onlyoneaman <2000.aman.sinha@gmail.com>
2025-03-05 13:59:58 +0530

    Tools

--- src/data/toolsData.ts
+++ src/data/toolsData.ts
@@ -1,52 +1,82 @@
 const toolsData = [
-    {
-        title: "ChatGPT",
-        tag: "Productivity",
-        image: "chatgpt.png",
-        link: "https://chat.openai.com/"
-    },
-    {
-        title: "Logseq",
-        tag: "Productivity",
-        image: "logseq.png",
-        link: "https://logseq.com/"
-    },
-    {
-        title: "Google Tasks",
-        tag: "Productivity",
-        image: "tasks.png",
-        link: "https://tasks.google.com/"
-    },
-    {
-        title: "Notion",
-        tag: "Productivity",
-        image: "notion.png",
-        link: "https://www.notion.so/"
-    },
-    {
-        title: "Buffer",
-        tag: "Post Scheduler",
-        image: "buffer.png",
-        link: "https://buffer.com/"
-    },
-    {
-        title: "Miro",
-        tag: "Collaboration",
-        image: "miro.png",
-        link: "https://miro.com/"
-    },
-    {
-        title: "Framer",
-        tag: "Design",
-        image: "framer.webp",
-        link: "https://www.framer.com/"
-    },
-    {
-        title: "Readwise Reader",
-        tag: "Reading",
-        image: "reader.png",
-        link: "https://readwise.io/"
-    }
+  {
+    title: "ChatGPT",
+    tag: "Productivity",
+    image: "chatgpt.png",
+    link: "https://chat.openai.com/"
+  },
+  {
+    title: 'Claude',
+    tag: 'Productivity',
+    image: 'claude.png',
+    link: 'https://claude.ai/'
+  },
+  // {
+  //     title: "Logseq",
+  //     tag: "Productivity",
+  //     image: "logseq.png",
+  //     link: "https://logseq.com/"
+  // },
+  // {
+  //     title: "Google Tasks",
+  //     tag: "Productivity",
+  //     image: "tasks.png",
+  //     link: "https://tasks.google.com/"
+  // },
+  {
+    title: "Notion",
+    tag: "Productivity",
+    image: "notion.png",
+    link: "https://www.notion.so/"
+  },
+  {
+    title: 'Webstorm',
+    tag: 'IDE',
+    image: 'webstorm.png',
+    link: 'https://www.jetbrains.com/webstorm/'
+  },
+  {
+    title: 'Pycharm',
+    tag: 'IDE',
+    image: 'pycharm.png',
+    link: 'https://www.jetbrains.com/pycharm/'
+  },
+  // {
+  //     title: "Buffer",
+  //     tag: "Post Scheduler",
+  //     image: "buffer.png",
+  //     link: "https://buffer.com/"
+  // },
+  // {
+  //     title: "Miro",
+  //     tag: "Collaboration",
+  //     image: "miro.png",
+  //     link: "https://miro.com/"
+  // },
+  // {
+  //     title: "Framer",
+  //     tag: "Design",
+  //     image: "framer.webp",
+  //     link: "https://www.framer.com/"
+  // },
+  {
+    title: "Readwise Reader",
+    tag: "Reading",
+    image: "reader.png",
+    link: "https://readwise.io/"
+  },
+  {
+    title: "Slack",
+    tag: "Communication",
+    image: "slack.png",
+    link: "https://slack.com/"
+  },
+  {
+    title: "Medium",
+    tag: "Writing",
+    image: "medium.png",
+    link: "https://medium.com/"
+  }
 ]
 
 export default toolsData;
